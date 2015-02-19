187137c1f96ff26439b3303fd9e988083fabb690
Jesseract <jlpearce@gmail.com>
2015-02-18 20:10:34 -0500

    pass survey number changes test

--- test/models/survey_test.rb
+++ test/models/survey_test.rb
@@ -4,4 +4,11 @@ class SurveyTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "survey number changes" do
+      assert_difference "Survey.count", 1 do
+      Survey.create(title: "No More Snow Days", description: "Seriously, enough of this")
+    end
+  end
+
+
 end
